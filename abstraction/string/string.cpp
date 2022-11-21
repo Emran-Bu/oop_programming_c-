2a0b5e6f23dae141401ced42089a9d813e01938e
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-11-21 13:21:10 +0600

    c++ data abstraction

---
+++ abstraction/string/string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s1 = "Hello";
+    char arr[6] = {'E', 'M', 'R', 'A', 'N'};
+    string s2 = string(arr);
+    cout<<s1 + " ";
+    cout<<s2<<endl;
+    return 0;
+}
